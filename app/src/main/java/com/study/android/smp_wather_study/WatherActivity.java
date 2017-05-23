f846274b675e879be7fa787893ca116548db8b66
Diachi25 <monone.cllc46@docomo.ne.jp>
2017-05-23 21:33:32 +0900

    加藤さんへ

---
+++ app/src/main/java/com/study/android/smp_wather_study/WatherActivity.java
@@ -0,0 +1,10 @@
+package com.study.android.smp_wather_study;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by tatsuya on 2017/05/23.
+ */
+
+public class WatherActivity extends AppCompatActivity {
+}
